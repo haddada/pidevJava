1a9eb6b72366371f168a21aa5e8ed7723f9373ef
user <user@admin>
2015-02-10 17:41:29 +0100

    UtilisateurDAO utilisateur

--- src/DAO/UtilisateurDAO.java
+++ src/DAO/UtilisateurDAO.java
@@ -6,10 +6,51 @@
 
 package DAO;
 
+import Entity.Utilisateur;
+import Interface.IUtilisateurDAO;
+import Technique.MyConnection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author user
  */
-public class UtilisateurDAO {
+public class UtilisateurDAO implements IUtilisateurDAO{
+
+    public UtilisateurDAO() {
+    }
+
     
+    @Override
+    public void insertUtilisateur(Utilisateur u) {
+        String req="insert into utilisateur values(?,?,?,?,?) ";
+        try {
+            PreparedStatement ps=MyConnection.getInstance().prepareStatement(req);
+            ps.setInt(1,u.getId());
+            ps.setString(2,u.getNom());
+            ps.setString(3,u.getPrenom());
+            ps.setString(4,u.getMail());
+            ps.setString(5,u.getPassword());
+            ps.execute();
+        } catch (SQLException ex) {
+             Logger.getLogger(UtilisateurDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    @Override
+    public void deleteUtilisateur(Utilisateur u) {
+        String req="delete from utilisateur where Id=?";
+        try {
+            PreparedStatement ps=MyConnection.getInstance().prepareStatement(req);
+            ps.setInt(1,u.getId());
+            ps.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(UtilisateurDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+ 
 }
