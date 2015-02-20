d81f5d619ec733fa92ed8b9796032b1b4e128f6f
haddada <haddadaseif@gmail.com>
2015-02-20 14:50:00 +0100

    SignUp try

--- src/GUI/SignUP.java
+++ src/GUI/SignUP.java
@@ -11,6 +11,22 @@ import com.restfb.types.Page;
 import com.restfb.types.User;
 import DAO.ClientDAO;
 import Entity.Client;
+import chrriis.dj.nativeswing.swtimpl.components.JWebBrowser;
+import chrriis.dj.nativeswing.swtimpl.components.WebBrowserAdapter;
+import chrriis.dj.nativeswing.swtimpl.components.WebBrowserNavigationEvent;
+import com.alee.laf.WebLookAndFeel;
+import com.restfb.FacebookClient.AccessToken;
+import java.io.IOException;
+import java.io.StringReader;
+import javax.swing.text.html.HTMLEditorKit;
+import javax.swing.text.html.parser.ParserDelegator;
+import javax.swing.*; 
+import javax.swing.JPanel;
+import java.awt.Dimension;
+import java.net.URL;
+import java.net.*;
+
+
 
 /**
  *
@@ -20,17 +36,44 @@ public class SignUP extends javax.swing.JFrame {
 
     /**
      * Creates new form LoginGuiFB
-     */
-    
+     *//*
+        public static Client currentUser;
+    // les attribus 
+    public static String API_KEY = "1544983695751636";
+    public static String SECRET = "c9dbaeca6ebc4cd614cf1e317e0720db";
+
+    public static String firstRequest = "https://graph.facebook.com/oauth/authorize?"
+    + "client_id="
+    + API_KEY
+    + "&redirect_uri=http://www.facebook.com/connect/login_success.html&"
+    + "scope=email,user_birthday";
+    public static String secondRequest = "https://graph.facebook.com/oauth/access_token?"
+            + "client_id="
+            + API_KEY
+            + "&redirect_uri=http://www.facebook.com/connect/login_success.html&"
+            + "client_secret=" + SECRET + "&code=";
+    public static String access_token = "CAACEdEose0cBAA7ibu82UcfUzQoqayD0iZA9cLbkqDVAYyMsbbC2ZCKSpq32s0SQTLlZCWTz4y4jpzvoTidOxha6dBUJ2YGH4Y2Ns27fIngSnrRFcqiK3ZAYeZA6WS4sAbHZCWFr0xwYy0MIJCaDXr3BQrG1pC6rCijgZAGskoZAb0Q7oKXS8FXAArrMKd8HJbpYzZArPgDKC1oD1C38ZBD5uNLWzmZAXrwBZAUZD";
+    public static boolean firstRequestDone = false;
+    public static boolean secondRequestDone = false;
+    final JFrame loginFrame = new JFrame();
+    public static FacebookClient facebookClient = null;
+    private String userName;
+    private String userMail;
+    public static boolean fbCnxEnd = false;
+    */
     ClientDAO userDAO ;
     public SignUP() {
         initComponents();
-           // DefaultFacebookClient is the FacebookClient implementation
-        // that ships with RestFB. You can customize it by passing in
-        // custom JsonMapper and WebRequestor implementations, or simply
-        // write your own FacebookClient instead for maximum control.
+       /* URL url = new URL("http://example.com");
+        HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+        connection.setRequestMethod("GET");
+        connection.connect();
+        int code = connection.getResponseCode();
+*/
 
     }
+
+    
 
     /**
      * This method is called from within the constructor to initialize the form.
@@ -42,19 +85,20 @@ public class SignUP extends javax.swing.JFrame {
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
-        jButton1 = new javax.swing.JButton();
-        jTextField2 = new javax.swing.JTextField();
-        jSeparator1 = new javax.swing.JSeparator();
-        jTextField1 = new javax.swing.JTextField();
-        jLabel2 = new javax.swing.JLabel();
-        jTextField5 = new javax.swing.JTextField();
-        jButton2 = new javax.swing.JButton();
+        jPanel2 = new javax.swing.JPanel();
         jTextField4 = new javax.swing.JTextField();
+        jLabel2 = new javax.swing.JLabel();
+        jTextField1 = new javax.swing.JTextField();
+        jButton2 = new javax.swing.JButton();
+        jTextField2 = new javax.swing.JTextField();
+        jButton1 = new javax.swing.JButton();
         jTextField3 = new javax.swing.JTextField();
+        jTextField5 = new javax.swing.JTextField();
+        jSeparator1 = new javax.swing.JSeparator();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jButton1.setText("Inscription");
+        jPanel2.setBackground(new java.awt.Color(241, 241, 241));
 
         jLabel2.setIcon(new javax.swing.ImageIcon("C:\\Users\\seif\\Desktop\\incription.PNG")); // NOI18N
 
@@ -65,39 +109,41 @@ public class SignUP extends javax.swing.JFrame {
             }
         });
 
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(91, 91, 91)
+        jButton1.setText("Inscription");
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addGap(81, 81, 81)
                         .addComponent(jSeparator1, javax.swing.GroupLayout.PREFERRED_SIZE, 323, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addContainerGap()
+                    .addGroup(jPanel2Layout.createSequentialGroup()
                         .addComponent(jLabel2)
                         .addGap(48, 48, 48)
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 191, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(jTextField4, javax.swing.GroupLayout.PREFERRED_SIZE, 191, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, 191, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, 191, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(jTextField5, javax.swing.GroupLayout.PREFERRED_SIZE, 191, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(114, 114, 114)
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addGap(104, 104, 104)
                         .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 264, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(135, 135, 135)
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addGap(125, 125, 125)
                         .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 231, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addContainerGap(97, Short.MAX_VALUE))
         );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel2Layout.createSequentialGroup()
                         .addGap(58, 58, 58)
                         .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(41, 41, 41)
@@ -115,7 +161,18 @@ public class SignUP extends javax.swing.JFrame {
                 .addComponent(jSeparator1, javax.swing.GroupLayout.PREFERRED_SIZE, 10, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 60, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(59, Short.MAX_VALUE))
+                .addContainerGap(81, Short.MAX_VALUE))
+        );
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel2, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -137,7 +194,9 @@ public class SignUP extends javax.swing.JFrame {
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         // TODO add your handling code here:
         userDAO=new ClientDAO();
-        FacebookClient facebookClient = new DefaultFacebookClient("CAACEdEose0cBAIns7Df1iZC9Gp64eAACsDwqfBzY6CbftsgdQrORt8kTTWjnTZC3Qo1Ef16G8a4T4nUTQgdtxz0hzSAYNeuSZAdKzgedbrZCCWDeXNVOiL5Cf0LTOTQAacSyLdZCGSMId5rG7DDllImcUx94USTRcmBfNN9ZCX1BcBmmFoTPuefHaSzySacpUGIi3YPTqQAHZBEwJ0qMZCffwQTs97Rv1TsZD");
+        AccessToken accessToken =
+        new DefaultFacebookClient().obtainAppAccessToken("433756370121014", "0274ae3496a60be4d7d21bf1c7d46b59");
+        FacebookClient facebookClient = new DefaultFacebookClient(accessToken.getAccessToken());
 
 // It's also possible to create a client that can only access
 // publicly-visible data - no access token required. 
@@ -197,6 +256,8 @@ public class SignUP extends javax.swing.JFrame {
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
+                WebLookAndFeel.install();
+                WebLookAndFeel.setDecorateAllWindows(true);
                 new SignUP().setVisible(true);
             }
         });
@@ -207,6 +268,7 @@ public class SignUP extends javax.swing.JFrame {
     private javax.swing.JButton jButton2;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
     private javax.swing.JSeparator jSeparator1;
     private javax.swing.JTextField jTextField1;
     private javax.swing.JTextField jTextField2;
